daae02dddd9f6eff423fd1dfcd184d25bcabe1cf
DAMIR DURMO <ddurmo@gmail.com>
2016-03-03 02:57:41 -0700

    minor changes

--- app/src/components/TwitterUser.jsx
+++ app/src/components/TwitterUser.jsx
@@ -3,12 +3,17 @@ import Immutable from 'immutable';
 import Panel from 'react-bootstrap/lib/Panel';
 import Tweets from './Tweets.jsx'
 export default class TwitterUser extends React.Component {
+  tweet0=0;
   constructor(props){
     super(props);
-    this.state= {
-      tweet: 0
+      this.state= {
+        tweet: this.tweet0
     };
   }
+  getTweets=(tweet) =>{
+    this.setState={tweet:tweet};
+    console.log(this.setState);
+}
   render() {
     let tweetNum=this.state.tweet;
     return (<div className="userinfo row"> {this.props.tweets.map(tweetUser=>
@@ -17,18 +22,15 @@ export default class TwitterUser extends React.Component {
       <h1>{tweetUser.handle}</h1>
       <Tweets tweets={tweetUser.tweets[tweetNum]}/>
       <div className="tweetControllers">
-        <a onClick={this.getTweets(0)}><i className="fa fa-circle"></i></a>
-        <a onClick={this.getTweets(1)}><i className="fa fa-circle"></i></a>
-        <a onClick={this.getTweets(2)}><i className="fa fa-circle"></i></a>
-        <a onClick={this.getTweets(3)}><i className="fa fa-circle"></i></a>
-        <a onClick={this.getTweets(4)}><i className="fa fa-circle"></i></a>
+        <a onClick={this.getTweets.bind(this, "0")}><i className="fa fa-circle"></i></a>
+        <a onClick={this.getTweets.bind(this, "1")}><i className="fa fa-circle"></i></a>
+        <a onClick={this.getTweets.bind(this, "2")}><i className="fa fa-circle"></i></a>
+        <a onClick={this.getTweets.bind(this, "3")}><i className="fa fa-circle"></i></a>
+        <a onClick={this.getTweets.bind(this, "4")}><i className="fa fa-circle"></i></a>
       </div>
     </Panel>
     )}</div>
    )
  }
 
-   getTweets=(tweet) =>{
-     this.setState= ({tweet});
-   }
 }
